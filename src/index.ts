4202989aeeb0e68c46b7be0880c230640a0527e5
woogieOnAndOn <123wodnr@naver.com>
2022-04-11 20:57:17 +0900

    [환경] cookie parser 설치

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,18 @@ import * as bodyParser from 'body-parser';
 import { interfaces, InversifyExpressServer, TYPE } from 'inversify-express-utils';
 import { container } from '../src/config/ioc.container'; 
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 import uploadRouter from "./file/upload.routes";
 
 // create server
 let server = new InversifyExpressServer(container);
 server.setConfig((app) => {
-  app.use(cors({origin: true}))
+  app.use(cors({
+    origin: 'http://localhost:5001',
+    credentials: true
+  }))
+  app.use(cookieParser());
   app.use("/upload", uploadRouter);
 
   // add body parser
